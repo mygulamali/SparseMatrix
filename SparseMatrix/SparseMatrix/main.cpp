537b3c7a374b41b3d9aa76aadadd2fa34454b178
Murtaza Gulamali <mygulamali@gmail.com>
2011-11-04 01:21:59 +0000

    Cleaning up Git.

---
+++ SparseMatrix/SparseMatrix/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SparseMatrix
+//
+//  Created by Murtaza Gulamali on 04/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
